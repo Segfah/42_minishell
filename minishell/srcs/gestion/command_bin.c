9ddffe94207fc4622b4f6aa7ec1582ae36ccc8db
corozco <corozco@student.42.fr>
2020-08-04 02:16:32 +0200

    update command_bin exit pour sortir du fork fils

--- minishell/srcs/gestion/command_bin.c
+++ minishell/srcs/gestion/command_bin.c
@@ -6,7 +6,7 @@
 /*   By: lryst <lryst@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/03 15:00:48 by lryst             #+#    #+#             */
-/*   Updated: 2020/08/04 01:04:00 by corozco          ###   ########.fr       */
+/*   Updated: 2020/08/04 02:15:57 by corozco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,52 +14,44 @@
 
 int     command_bin(char **tab)
 {
- //   char **tab;
-    char *new_env[] = {NULL};
-    //printf("str = [%s]\n", str);
-    char *command;
-    int i;
-    pid_t f;
+	char *new_env[] = {NULL};
+	char *command;
+	int i;
+	pid_t f;
 
-    f = fork();
-    i = 0;
-    if (!(command = (char*)malloc(sizeof(char) * ft_strlen(tab[0]) + 6)))
-        return(0);
-    command[0] = '/';
-    command[1] = 'b';
-    command[2] = 'i';
-    command[3] = 'n';
-    command[4] = '/';
-    while (tab[0][i] != '\0')
-    {
-        command[i + 5] = tab[0][i];
-        i++;
-    }
-    command[i + 5] = '\0';
-    //printf("command = [%s]\n", command);
-/*    int a;
-    a = 0;
-    while (tab[a] != NULL)
-    {
-        printf("tab[%d] = {%s}\n", a , tab[a]);
-        a++;
-    }
-    a = 0;
-    while (envp[a] != NULL)
-    {
-        printf("envp[%d] = {%s}\n", a , envp[a]);
-        a++;
-    } */
-    if (f == 0)
-    {
-        if (execve(command, tab, new_env) == -1)
-        {
-            free(command);
-            return (1);
-        }
-        return(1);
-    }
-    wait(&f);
-    free(command);
-    return (0);
+	f = fork();
+	i = 0;
+	if (!(command = (char*)malloc(sizeof(char) * ft_strlen(tab[0]) + 6)))
+		return(0);
+	command[0] = '/';
+	command[1] = 'b';
+	command[2] = 'i';
+	command[3] = 'n';
+	command[4] = '/';
+	while (tab[0][i] != '\0')
+	{
+		command[i + 5] = tab[0][i];
+		i++;
+	}
+	command[i + 5] = '\0';
+	//printf("command = [%s]\n", command);
+/*	int a;
+	a = 0;
+	while (tab[a] != NULL)
+	{
+		printf("tab[%d] = {%s}\n", a , tab[a]);
+		a++;
+	}
+	} */
+	if (f == 0)
+	{
+		if (execve(command, tab, new_env) == -1)
+		{
+			free(command);
+			exit(0);
+		}
+	}
+	wait(&f);
+	free(command);
+	return (0);
 }
