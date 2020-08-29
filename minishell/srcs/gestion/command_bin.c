2bc1ae544093558e38e6df2b2380f56916fc1783
corozco <corozco@student.42.fr>
2020-08-30 01:44:02 +0200

    bin + path

--- minishell/srcs/gestion/command_bin.c
+++ minishell/srcs/gestion/command_bin.c
@@ -6,26 +6,12 @@
 /*   By: lryst <lryst@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/08/03 15:00:48 by lryst             #+#    #+#             */
-/*   Updated: 2020/08/29 05:09:36 by corozco          ###   ########.fr       */
+/*   Updated: 2020/08/30 01:43:34 by corozco          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
-
-/*
-char	**create_env_exec(t_lists *list)
-{
-	t_lists *tmp;
-	char	tab
-
-	tmp = list;
-	while (tmp != NULL)
-	{
-		tmp = tmp->next;
-	}
-	return (
-}
-*/
+#include <sys/stat.h>
 
 static char		**build_cmd(t_temp *tmp, char *cmd, int *a)
 {
@@ -47,47 +33,51 @@ static char		**build_cmd(t_temp *tmp, char *cmd, int *a)
 		if (!(tabpath[*a] = ft_strjoin(tabpath[*a], cmd)))
 			return (NULL);
 		free(temp);
-	//	ft_printf("[%s]\n", tabpath[*a]);
 	}
 	free(path);
 	return (tabpath);
+}
+
+
+int				cmd_is_here(char **path)
+{
+	int			i;
+	struct stat	stats;
+
+	i = -1;
+	while (path[++i])
+	{
+		if (stat(path[i], &stats) == 0)
+			return (i);
+	}
+	return (-1);
 }
 
 int				command_bin(char **tab, t_temp *tmp)
 {
 	char		*new_env[] = {NULL};
 	pid_t		f;
-	pid_t		w;
+//	pid_t		w;
 	int			status;
 	char		**tabpath;
 	int			i;
+	int			s_bin;
 
 	i = -1;
-	if (!(tabpath = build_cmd(tmp, tab[0], &i)))
+	if ((search_env("PATH", tmp, 1, NULL) == 0)
+		|| !(tabpath = build_cmd(tmp, tab[0], &i)))
+		return (-1);
+	if ((s_bin =cmd_is_here(tabpath)) == -1)
 		return (-1);
 	f = fork();
 	if (f == 0)
 	{
-		int intento = 0;
-		int returno = 0;
-		while (tabpath[intento])
-		{
-			if ((returno = execve(tabpath[intento++], tab, new_env)) == 0)
-				exit(0);
-		}
-		exit(1);
-	}
-	else
-	{
-		if ((w = waitpid(f, &status, WUNTRACED | WCONTINUED)) == -1)
+		if (execve(tabpath[s_bin], tab, new_env) == -1)
 			exit(1);
-		if (WIFEXITED(status))
-			if (WEXITSTATUS(status) == 1)
-			{
-				ft_free_split(tabpath, i);
-				return (-1);
-			}
+		exit(0);
 	}
+	if ((f = waitpid(f, &status, WUNTRACED | WCONTINUED)) == -1)
+		exit(1);
 	ft_free_split(tabpath, i);
 	return (0);
 }
