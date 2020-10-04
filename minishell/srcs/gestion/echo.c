d6950b71144cf10c11260bc67a8c48a33c4f12fd
Lucie RYST <lryst@e1r3p10.42.fr>
2020-10-04 18:55:42 +0200

    gestion -nnnnn OK

--- minishell/srcs/gestion/echo.c
+++ minishell/srcs/gestion/echo.c
@@ -6,47 +6,30 @@
 /*   By: lryst <lryst@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/10 17:27:31 by lryst             #+#    #+#             */
-/*   Updated: 2020/10/03 23:12:14 by lryst            ###   ########.fr       */
+/*   Updated: 2020/10/04 18:54:07 by lryst            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-int		check_option_echo(char *arg)
+int		ft_check_option_echo(char *s)
 {
 	int i;
 	int len;
 
-	len = ft_strlen(arg);
 	i = 0;
-	if (arg[i] == '-' && arg[i + 1] == 'n')
+	len = ft_strlen(s);
+	if (s[i] == '-')
 	{
-		while
+		if (s[i + 1])
+			i++;
+		while (s[i] && s[i] == 'n')
+			i++;
 	}
-}
-
-char 	*gestion_echo_option(char *str, char *arg)
-{
-	char *cmd;
-	int i;
-	int j;
-	int save;
-
-	i = 4;
-	j = 0;
-	if (ft_strcmp(arg, "-n") == 0)
-		ft_strcmp(arg, "-n") == 0 ? i = 8 : 0;
-	save = i;
-	while (str[i++] != '\0')
-		j++;
-	if (!(cmd = (char*)malloc(sizeof(char) * j + 1)))
-		return (NULL);
-	i = save;
-	j = 0;
-	while (str[i] != '\0') 
-		cmd[j++] = str[i++];
-	cmd[j] = '\0';
-	return (cmd);
+	printf("len = %d | i = %d | total = %d\n", len, i, len - 1);
+	if (i == len)
+		return (i);
+	return (0);
 }
 
 void    gestion_echo(t_cmd *cmd)
@@ -62,7 +45,7 @@ void    gestion_echo(t_cmd *cmd)
 		n = 1;
 	if (test->next != NULL)
 		test = test->next;
-	if (ft_strcmp(test->output, "-n") == 0)
+	if (ft_check_option_echo(test->output) != 0)
 	{
 		test = test->next;
 		if (test != NULL)
