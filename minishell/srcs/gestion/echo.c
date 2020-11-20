eb0e4c0415cb00e49ca43e53974d8326a996d992
R <lu@MacBook-Air-de-R.local>
2020-11-20 17:44:54 +0100

    echo' ' gerer

--- minishell/srcs/gestion/echo.c
+++ minishell/srcs/gestion/echo.c
@@ -6,7 +6,7 @@
 /*   By: lryst <lryst@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/10 17:27:31 by lryst             #+#    #+#             */
-/*   Updated: 2020/11/19 12:19:11 by lryst            ###   ########.fr       */
+/*   Updated: 2020/11/20 17:42:19 by lryst            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,71 @@ void		dup_or_not(t_temp *tmp)
 	}
 }
 
+char	*ft_newstring(size_t i)
+{
+	char	*str;
+	size_t	zero;
+
+	zero = 0;
+	if (!(str = (char*)malloc(sizeof(char) * i + 1)))
+		return (NULL);
+	while (str && zero < i + 1)
+	{
+		str[zero] = '\0';
+		zero++;
+	}
+	return (str);
+}
+
+char	*ft_strjoinfree(char *s1, char *s2)
+{
+	char	*tab;
+	int		i;
+	int		u;
+
+	i = 0;
+	u = 0;
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
+	tab = (char*)malloc(sizeof(char) * (ft_strlen((char *)s1) +
+				ft_strlen((char *)s2)) + 1);
+	if (tab == NULL)
+		return (NULL);
+	while (s1[i])
+	{
+		tab[i] = s1[i];
+		i++;
+	}
+	while (s2[u])
+		tab[i++] = s2[u++];
+	tab[i] = '\0';
+	free(s1);
+	s1 = NULL;
+	return (tab);
+}
+
+int			echo_cote_space(t_temp *tmp)
+{
+	int i;
+	int count;
+	char *str;
+
+	i = 0;
+	count = 0;
+	str = ft_newstring(0);
+	if (tmp->strcmdin[1] && ((ft_check_option_echo(tmp->strcmd[1]) != 0) ||
+	ft_strcmp(tmp->strcmdin[count]," ") == 0))
+		return (1);
+	while (tmp->strcmdin[count] && (ft_strcmp(tmp->strcmdin[count]," ") != 0))
+	{
+		str = ft_strjoinfree(str, tmp->strcmd[count]);
+		count++;
+	}
+	ft_printf("minishell: %s: command not found\n", str);
+	ft_free(str);
+	return (0);
+}
+
 void		gestion_echo(t_temp *tmp)
 {
 	int		i;
@@ -60,12 +125,15 @@ void		gestion_echo(t_temp *tmp)
 	i = 0;
 	n = 0;
 	dup_or_not(tmp);
+	if (echo_cote_space(tmp) == 0)
+		return ;
 	if (tmp->strcmd[i + 1] != NULL)
 		i++;
 	else
 		n = 1;
 	if (tmp->strcmd[i + 1] != NULL)
 		i++;
+	
 	if (ft_check_option_echo(tmp->strcmd[i]) != 0)
 	{
 		i++;
