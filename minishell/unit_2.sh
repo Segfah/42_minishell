f1a73e3914fd86ccf44c12e62ddf17c38b967554
corozco <corozco@student.42.fr>
2020-11-29 06:26:07 +0100

    git add unit_2.sh

--- minishell/unit_2.sh
+++ minishell/unit_2.sh
@@ -11,17 +11,18 @@ TEST_ARRAY=(
 'echo hola buenos dias | cat -e'
 'echo -n hola que tal bocadillo digital | cat -e'
 #### ENV ####
-#'env'
+'env | grep USER'
 #### EXPORT ####
 'export Z=z ; echo $Z'
 'export A=a B=b C=c; echo $A$B$C'
+'export A=mi-moto B=alpino C=derrapante; echo $A$B$C'
 'export zz zzz= zzzz=asd ; echo $zz$zzz$zzzz; export | grep zz'
-###############'export =a ; echo $a'
-'export $HOME=hola ; echo $HOME'
-###############'export $M=3 ; echo $M'
-###############'export _=a; echo $_'
+'export =a ; echo $a'
+'export /dont/export/this=hola ; export | grep /dont/export/this'
 'export A=a=a=a=a=a; echo $A'
 'export A B C; echo $A$B$C'
+'export $'
+'export ?=42'
 #### UNSET ####
 'unset'
 'export A=a ; unset A ; echo $A'
@@ -31,7 +32,8 @@ TEST_ARRAY=(
 'unset HOME ; cd ; pwd'
 'cd .; pwd'
 'cd ..; pwd'
-
+#'cd ..; echo $OLDPWD; pwd'
+#'unset OLDPWD; cd .. ; echo $OLDPWD; pwd'
 "cd ' /'; pwd"
 'cd ../../ ; pwd'
 'cd ../../../../../../.. ; pwd'
@@ -43,10 +45,12 @@ TEST_ARRAY=(
 'exit 512'
 'exit 1407'
 'exit 21 42'
-'exit $HOME"coucou"COMO" "TE" "ENCUENTRAS |rev'
-'exit $HOME"coucou"COMO" "TE" "ENCUENTRAS'
-'pwd | exit $HOME"coucou"COMO" "TE" "ENCUENTRAS'
-'pwdd | exit $HOME"coucou"COMO" "TE" "ENCUENTRAS'
+'################	NO ENVIRONMENT (env -i ./minishell)	#################'
+'cd'
+'pwd'
+'echo $PWD'
+'cd .. ; echo $OLDPWD'
+'./lscp'
 '################		COMMAND EXECUTION	        #################'
 'ls'
 '/bin/ls'
@@ -69,13 +73,14 @@ TEST_ARRAY=(
 "echo '\$HOME'"
 "echo ' \"\$HOME\" '"
 "echo \"'\$HOME'\""
-"echo \" '\$PWD' \\\"\$PWD\\\" '\$PWD' \""
-"echo \"\\\$HOME\""
+"echo \" '\$PWD' \\\"\$PWD\\\" '\$PWD' \"" "echo \"\\\$HOME\""
+#"echo \"\\\''\'\""
 "echo \"'\$'\""
 "echo \\\\\n"
-"echo \"coucou ca va ? '\$HOME' \coucou\""
 "echo \"< no pipe | or semicolon will ; stop me >\""
-'\"asdasdasd\"$asdasd$HOME'
+#'bash -c "I am not a command" "Im the program name"'
+'pwd" should not work"'
+'echo\" should not work neiter\"'
 '################		    PIPES			#################'
 'echo 5 + 3 | bc'
 'ls | wc | wc -l | bc'
@@ -102,17 +107,14 @@ TEST_ARRAY=(
 'notacommand >> a'
 'pwd >> dir'
 'ls >> a imnotaflag meneither'
-#'################	    LEFT REDIRECTION		    #################'
-'/'
-'./'
-'/.'
-'echo\" asdasdasd\"$asdasd$HOME coucou'
-'\"asdasdasd\"$asdasd$HOME'
-'exit 42\"asdasdasd\"$asdasd$HOME coucou'
-'exit 42"asdasdasd"$asdasd$HOME coucou'
-"echo \'"
-"echo '\'"
-"echo \"\\\''\'\""
+'################	    LEFT REDIRECTION			#################'
+'echo pim pam > pum ; cat < pum'
+'touch a b c; echo sorry > d; cat < a < b < c < d'
+'echo ayayay > a ; cat < doesnotexist < a'
+'cat < doesnotexist'
+'cat < dir'
+'pwd > a < b ; ls ; cat a'
+'pwd < a > b ; ls ; cat b'
 )
 
 GREEN=$(tput setaf 2)
@@ -127,16 +129,16 @@ COLORBONITO=$(tput setaf 12)
 NC=$(tput sgr0)
 
 
-
-
-make
+make 
 cp ./the_big_minishell ./minishell
 printf "copying your minishell to the current directory...\n"
 
 rm -rf diff.txt
 
-mkdir dir dir/encoreuneautredir
-printf "creating directories ${ROSITA}dir$NC, ${ROSITA}dir/encoreuneautredir$NC...\n"
+if  [[ ! -d dir || ! -d dir/encoreuneautredir ]]; then
+    mkdir dir dir/encoreuneautredir
+    printf "creating directories ${ROSITA}dir$NC, ${ROSITA}dir/encoreuneautredir$NC...\n"
+fi
 if [[ ! -d dirwithoutpermissions ]]; then
     mkdir -m 0000 dirwithoutpermissions
     printf "creating directory ${ROSITA}dirwithoutpermissions$NC...\n"
@@ -149,17 +151,25 @@ printf "\n\t\t\t    ${YELLOW}[ MINISHELL UNIT TEST ]$NC\n\n\n"
 
 TOTAL=0
 PASSED=0
+ENV=""
 for val in "${TEST_ARRAY[@]}"
 do
-    if [[ "$val" == *####* ]]; then
+    if [[ "$val" = *ENVIRONMENT* ]]
+    then
+	ENV="env -i"
+    elif [[ "$val" = *EXECUTION* ]]
+    then
+	ENV=""
+    fi
+    if [[ "$val" = *####* ]]; then
 	printf "%s" "${NC}$val"
 	printf " ${COLORBONITO}STDERR$NC\n"
 	continue 
     fi
-    bash -c "$val" minishell > out1 2> err1
+    $ENV bash -c "$val" minishell > out1 2> err1
     RET1=$?
     rm -rf a b c d
-    ./minishell -c "$val" > out2 2> err2
+    $ENV ./minishell -c "$val" > out2 2> err2
     RET2=$?
     rm -rf a b c d
     if [[ $(uname) == "Darwin" ]]; then
@@ -172,10 +182,13 @@ do
     fi
     DIFF=$(diff out1 out2) 
     ERRDIFF=$(diff err1 err2)
-
     if [[ "$DIFF" != "" || $RET1 != $RET2 || $ERRDIFF != ""  ]]
     then
-	printf "%s\n" "${YELLOW}$val$NC" >> diff.txt
+	if [[ $ENV == "" ]]; then
+	    printf "%s\n" "${YELLOW}$val$NC" >> diff.txt
+	else
+	    printf "%s\n" "${YELLOW}$ENV ./minishell: $val$NC" >> diff.txt
+	fi
 	printf "${ROSITA}< bash       (exited with %d)$NC\n" "$RET1" >> diff.txt
 	printf "${ROSITA}> minishell  (exited with %d)\n$NC" "$RET2" >> diff.txt
     fi
@@ -219,6 +232,6 @@ printf "  ||  ${GREEN}$PASSED passed$NC - ${RED}$((TOTAL - PASSED)) failed$NC"
 
 printf "\n\n\t\t\'cat diff.txt | less\'  for detailed information\n\n"
 
-rm -rf minishell out1 out2 err1 err2 a b c d lscp
+rm -rf minishell out1 out2 err1 err2 a b c d pum lscp
 chmod +r dirwithoutpermissions
 rm -rf ucantexecme.e dir dir/encoreuneautredir dirwithoutpermissions
